bef8a6ba6444651c17566cd1c20026e6a19c6e94
cw-roy <curtis_roy@msn.com>
2023-09-06 14:28:02 -0700

    Add buttons and image

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -1,21 +1,36 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
 const HeroSection = () => {
-	return (
-		<section>
-			<div className='grid grid-cols-1 lg:grid-cols-12'>
-				<div className='col-span-7 place-self-center'>
-					<h1 className='text-white mb-4 text-4xl lg:text-6xl font-extrabold'>
-						Hello, I'm Curtis
-					</h1>
-					<p className='text-[#ADB7BE] text-lg lf:text-xl'>
-						This will be some text
-					</p>
-				</div>
-				<div className='col-span-5'></div>
-			</div>
-		</section>
-	)
-}
+  return (
+    <section>
+      <div className="grid grid-cols-1 lg:grid-cols-12">
+        <div className="col-span-7 place-self-center text-center sm:text-left">
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+            Hello, I'm Curtis 
+          </h1>
+          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lf:text-xl">
+            This will be some text
+          </p>
+          <div>
+            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-600 via-purple-600 to-orange-600 hover:bg-slate-200 text-white">More Info</button>
+            <button className="px-6 py-3 w-full sm:w-fit rounded-full bg-transparent hover:bg-slate-800 text-white border border-white mt-3 ">Download CV</button>
+          </div>
+        </div>
+        <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+            <Image
+              src="/images/hero-image.png"
+              alt="hero image"
+              className="rounded-full absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              width={300}
+              height={300}
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default HeroSection
+export default HeroSection;
--- app/page.js
+++ app/page.js
@@ -1,10 +1,13 @@
-import Image from 'next/image'
-import HeroSection from './components/HeroSection'
+import Image from "next/image";
+import HeroSection from "./components/HeroSection";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212] container mx-auto px-12 py-4">
-      <HeroSection />
+      
+      <div class="container mt-4 mx-auto px-12 py-4">
+        <HeroSection />
+      </div>
     </main>
-  )
+  );
 }
