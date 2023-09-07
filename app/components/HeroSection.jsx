6ae3e271d18965d1c851942129576c04683aac98
cw-roy <curtis_roy@msn.com>
2023-09-07 13:19:16 -0700

    Add animation and styling

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -1,20 +1,62 @@
+"use client";
+
+// HeroSection.jsx
+
 import React from "react";
 import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
 
 const HeroSection = () => {
   return (
     <section>
+      <h1 className="px=6 py-6 text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+        <span className="text-transparent bg-clip-text bg-gradient-to-br from-indigo-600 via-purple-600 to-pink-600 ">
+          Hello! I&apos;m Curtis, and
+        </span>
+      </h1>
       <div className="grid grid-cols-1 lg:grid-cols-12">
         <div className="col-span-7 place-self-center text-center sm:text-left">
-          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
-            Hello, I&apos;m Curtis 
-          </h1>
-          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lf:text-xl">
-            This will be some text
-          </p>
+          <div></div>
           <div>
-            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-600 via-purple-600 to-orange-600 hover:bg-slate-200 text-white">More Info</button>
-            <button className="px-6 py-3 w-full sm:w-fit rounded-full bg-transparent hover:bg-slate-800 text-white border border-white mt-3 ">Download CV</button>
+            <h1 className="text-white mb-4 text-2xl sm:text-3xl lg:text-4xl font-extrabold">
+              <TypeAnimation
+                sequence={[
+                  // "Curtis",
+                  // 1000,
+                  "I'm an aficionado of all things tech,",
+                  1000,
+                  "Scrum.org certified (PSM I and PSPO I),",
+                  1000,
+                  "a Level 2 Desktop support consultant,",
+                  1000,
+                  "an AWS Certified Cloud Practitioner,",
+                  1000,
+                  // "Curtis",
+                  // 1000,
+                  "a fan of Python,",
+                  1000,
+                  "Linux Professional Institute certified,",
+                  1000,
+                  "having fun learning JavaScript,",
+                  250,
+                  "and HTML,",
+                  250,
+                  "and CSS,",
+                  1000,
+                  // "Curtis",
+                  // 1000,
+                  "an aspiring Full Stack engineer,",
+                  1000,
+                  "gaining experience with the MERN stack.",
+                  1000,
+                ]}
+                wrapper="span"
+                speed={50}
+                style={{ display: "inline-block" }}
+                omitDeletionAnimation={true}
+                repeat={Infinity}
+              />
+            </h1>
           </div>
         </div>
         <div className="col-span-5 place-self-center mt-4 lg:mt-0">
@@ -28,6 +70,19 @@ const HeroSection = () => {
             />
           </div>
         </div>
+      </div>
+      <div className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
+        <p>This will be some text</p>
+      </div>
+      <div>
+        <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-indigo-600 via-purple-600 to-pink-600 hover:bg-slate-200 text-white">
+          More Info
+        </button>
+        <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-indigo-600 via-purple-600 to-pink-600 hover:bg-slate-800 text-white mt-3 ">
+          <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2 ">
+            New Thing
+          </span>
+        </button>
       </div>
     </section>
   );
