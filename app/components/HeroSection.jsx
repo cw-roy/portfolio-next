ad0dd741ce5996db7f665aec8c6db46f13b6f6bf
cw-roy <curtis_roy@msn.com>
2023-09-19 16:50:24 -0700

    Add new sections

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -70,7 +70,7 @@ const HeroSection = () => {
             <Image
               src="/images/hero-image.png"
               alt="hero image"
-              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              className="rounded-full absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               width={300}
               height={300}
             />
